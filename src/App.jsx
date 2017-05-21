4447217f8b283649780a2e7073eabd7635bbaa14
Serhii Kondratiuk <vielon.indie@yandex.ru>
2017-05-21 20:13:18 +0300

    musicactors-0.2-sptify-api-endpoint-start

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,19 @@ import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            query: ''
+        }
+    }
+
+    search() {
+        console.log('this.state', this.state);
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+        console.log('FETCH_URL', FETCH_URL);
+    }
     render() {
         return (
             <div className="app">
@@ -12,8 +25,15 @@ class App extends Component {
                         <FormControl
                             type="text"
                             placeholder="Search for an artist"
+                            value={this.state.query}
+                            onChange={event => {this.setState({query: event.target.value})}}
+                            onKeyPress={event => {
+                            if (event.key === 'Enter') {
+                                this.search()
+                            }}
+                            }
                         />
-                        <InputGroup.Addon>
+                        <InputGroup.Addon onClick={() => this.search()}>
                             <Glyphicon glyph="search"></Glyphicon>
                         </InputGroup.Addon>
                     </InputGroup>
