4e57fe78d44716a8334607f2ee20c2777cfa40ac
Serhii Kondratiuk <vielon.indie@yandex.ru>
2017-05-24 10:15:10 +0300

    musicactors-0.3-adding-styles-and-improving-the-api

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
 import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import Profile from './Profile'
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            query: ''
+            query: '',
+            artist: null,
+            tracks: []
         }
     }
 
     search() {
         console.log('this.state', this.state);
         const BASE_URL = 'https://api.spotify.com/v1/search?';
-        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
-        console.log('FETCH_URL', FETCH_URL);
+        let FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+        const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
+
+        fetch(FETCH_URL, {
+            method: 'GET'
+            })
+            .then(response => response.json())
+            .then(json => {
+                const artist = json.artists.items[0];
+                console.log('artist', artist);
+                this.setState({artist});
+
+                FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+                fetch(FETCH_URL, {
+                    method: 'GET'
+                })
+                .then(response => response.json())
+                .then(json => {
+                    console.log('artist\'s top tracks:', json);
+                    const { tracks } = json;
+                    this.setState({tracks});
+                })
+            });
     }
     render() {
         return (
@@ -38,13 +62,19 @@ class App extends Component {
                         </InputGroup.Addon>
                     </InputGroup>
                 </FormGroup>
-                <div className="profile">
-                    <div>Artist Picture</div>
-                    <div>Artist name</div>
-                </div>
-                <div className="gallery">
-                    Gallery
-                </div>
+                {
+                    this.state.artist !== null
+                    ?
+                    <div>
+                        <Profile
+                            artist={this.state.artist}
+                        />
+                        <div className="gallery">
+                            Gallery
+                        </div>
+                    </div>
+                    : <div></div>
+                }
             </div>
         )
     }
